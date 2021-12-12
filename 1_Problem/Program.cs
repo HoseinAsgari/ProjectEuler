179dd25c57bc84ea5381f27f509b1c70065b7d8d
HoseinAsgari <hosein.asgari.00@gmail.com>
2021-12-12 15:27:59 +0330

    Solved first problem

--- 1_Problem/Program.cs
+++ 1_Problem/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _1_Problem
+namespace _1_Problem;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> rightNumbers = GetRightNumbers();
+
+        int sumOfNumbers = rightNumbers.Sum();
+
+        Console.WriteLine("Answer of the first problem: {0}", sumOfNumbers);
+    }
+
+    /// <summary>
+    /// Returns list of numbers that are natural, less than 1000 and multiple of 3 or 5.
+    /// </summary>
+    private static List<int> GetRightNumbers()
+    {
+        List<int> numbers = new();
+
+        for (int i = 1; i < 1000; i++)
         {
-            Console.WriteLine("Hello World!");
+            if (HasNumberTrueCondition(i))
+            {
+                numbers.Add(i);
+            }
+        }
+
+        return numbers;
+    }
+
+    /// <summary>
+    /// Return whether condition of problem is true or not for a number.
+    /// </summary>
+    /// <params>
+    /// Gets natural integer less than 1000.
+    /// </params>
+    private static bool HasNumberTrueCondition(int number)
+    {
+        if (number % 3 == 0 || number % 5 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
